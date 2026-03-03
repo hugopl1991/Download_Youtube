ba4ba3159672a2d18b0147be996d41375b48d812
Hugo <lima-hugo@hotmail.com>
2026-03-03 10:59:20 -0300

    Add todos dos script criados para realizar o download do youtube

---
+++ download_audio.py
@@ -0,0 +1,17 @@
+import yt_dlp
+
+url = "https://www.youtube.com/live/HRyBolh4xqs"
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(title)s.%(ext)s',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
+print("Download completed.")
---
+++ download_video.py
@@ -0,0 +1,13 @@
+import yt_dlp
+
+url = "https://www.youtube.com/live/HRyBolh4xqs"
+
+ydl_opts = {
+    'format': 'bestvideo+bestaudio/best',
+    'merge_output_format': 'mp4',
+    'outtmpl': '%(title)s.%(ext)s',
+}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
+print("Download completed.")
